e7a8ddf35ab5c65e7079250b6809ef63e854801b
albertgoncalves <goncalves.albert@gmail.com>
2019-04-02 19:05:25 -0400

    moving things around, more pervasives

--- src/main.ml
+++ src/main.ml
@@ -7,6 +7,18 @@ module Y = Sys
 type 'a count = ('a * int)
 type param = Alpha | RevAlpha | Error | Reverse | Delimiter of string
 
+let usage : string =
+    "HistOCaml
+tiny tool that tallies things (written in OCaml)
+
+USAGE   hist [-a -b -r] [-d DELIMITER]
+FLAGS   -a              sort output alphabetically
+        -b              sort output reverse alphabetically
+        -r              reverse default output
+        -d DELIMITER    replace default delimiter (\\t) with given string
+INPUT   stdin
+OUTPUT  stdout"
+
 let (|.) (f : 'b -> 'c) (g : 'a -> 'b) : ('a -> 'c) = fun x -> f (g x)
 
 let uncurry (f : 'a -> 'b -> 'c) (ab : ('a * 'b)) : 'c =
@@ -23,8 +35,7 @@ let read_lines : (unit -> string list) =
     loop [] |. read_line
 
 let histogram : 'a list -> 'a count list =
-    let rec loop (n : int) (accu : 'a count list)
-        : 'a list -> 'a count list =
+    let rec loop (n : int) (accu : 'a count list) : 'a list -> 'a count list =
         function
             | [] -> []
             | [x] -> (x, n)::accu
@@ -35,12 +46,10 @@ let histogram : 'a list -> 'a count list =
                     loop 1 ((x, n)::accu) xs in
     loop 1 [] |. L.fast_sort compare
 
-let unpack (xs : 'a count) : int = let (_, n) = xs in n
-
 let compare_hist (k : int) (a : 'a count) (b : 'a count) : int =
-    k * compare (unpack a) (unpack b)
+    k * compare (snd a) (snd b)
 
-let show_list (delimiter : string) (f : 'a -> string) : 'a list -> string =
+let show_hist (delimiter : string) (f : 'a -> string) : 'a list -> string =
     (P.sprintf "COUNT%sELEM\n%s" delimiter) |.  S.concat "\n" |. L.rev_map f
 
 let hist_to_string (delimiter : string) (xs : string count) : string =
@@ -51,22 +60,10 @@ let args () : string list = L.tl @@ A.to_list Y.argv
 let pipeline (delimiter : string) (f : 'a count list -> 'a count list)
     : (unit -> unit) =
     print_endline
-    |. show_list delimiter (hist_to_string delimiter)
+    |. show_hist delimiter (hist_to_string delimiter)
     |. f
     |. histogram
     |. read_lines
-
-let usage : string =
-"HistOCaml
-tiny tool that tallies things (written in OCaml)
-
-USAGE   hist [-a -b -r] [-d DELIMITER]
-FLAGS   -a              sort output alphabetically
-        -b              sort output reverse alphabetically
-        -r              reverse default output
-        -d DELIMITER    replace default delimiter (\\t) with given string
-INPUT   stdin
-OUTPUT  stdout"
 
 let parse : (string list -> param list) =
     let rec loop accu = function
@@ -93,7 +90,9 @@ let control_panel (delimiter : string) : (param list -> unit) =
         | Alpha::_ -> pipeline' (fun x -> x) ()
         | RevAlpha::_ -> pipeline' L.rev ()
         | Reverse::_ -> pipeline' (L.fast_sort @@ compare_hist @@ -1) ()
-        | Error::_ -> (fun _ -> exit 1) (print_endline usage)
+        (* NOTE: additional [-d ...] flags will throw an error! *)
+        | Error::_ | (Delimiter _)::_ ->
+            (fun _ -> exit 1) @@ print_endline usage
         | _ -> pipeline' (L.fast_sort @@ compare_hist 1) ()
 
 let main : (unit -> unit) =
